8a3a5c309e743c5ab982b21c6c7b12fb0edaf8b9
Ismaïl <nguyen.ismail@gmail.com>
2017-10-30 14:36:20 +0100

    Iteration 1 : Re initialize empty project from scratch

---
+++ tests/CoffeeMachineTests/CoffeeMachineTest.cs
@@ -0,0 +1,14 @@
+﻿using NFluent;
+using NUnit.Framework;
+
+namespace CoffeeMachineTests
+{
+    class CoffeeMachineTest
+    {
+        [Test]
+        public void Test()
+        {
+            Check.That(true).IsTrue();
+        }
+    }
+}
